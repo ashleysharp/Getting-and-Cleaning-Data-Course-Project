fcbd38b850a0bf4c10cb6f8f44fb19619d9c97ab
Ashley Sharp <ashley.sharpuk@gmail.com>
2016-10-18 18:53:20 +0100

    This is my first commit

---
+++ run_analysis.R
@@ -0,0 +1,93 @@
+setwd("/Users/ashleysharp/Dropbox/R/Coursera/gettingcleaning/UCI HAR Dataset")
+
+x_test <- read.table("test/X_test.txt", header=FALSE)
+y_test <- read.table("test/y_test.txt", header=FALSE)
+subject_test <- read.table("test/subject_test.txt", header=FALSE)
+
+x_train <- read.table("train/X_train.txt", header=FALSE)
+y_train <- read.table("train/y_train.txt", header=FALSE)
+subject_train <- read.table("train/subject_train.txt", header=FALSE)
+
+features <- read.table("features.txt", header=FALSE)
+activity_labels <- read.table("activity_labels.txt", header=FALSE)
+
+x_combined <- rbind(x_train, x_test)
+y_combined <- rbind(y_train, y_test)
+subject_combined <- rbind(subject_train, subject_test)
+
+rm(subject_test, subject_train, x_test, x_train, y_test, y_train)
+
+names(x_combined) <- features$V2
+names(y_combined) <- "activity_label"
+names(subject_combined) <- "subject"
+names(activity_labels) <- c("activity_label", "activity")
+
+xy_combined <- cbind(y_combined, x_combined)
+sxy_combined <- cbind(subject_combined, xy_combined)
+ID <- c(1:10299)
+id_combined <- cbind(ID, sxy_combined)
+
+all_combined <- merge(activity_labels, id_combined, by = "activity_label")
+all_combined <- arrange(all_combined, ID)
+all_combined <- select(all_combined, ID, subject, activity_label, activity, 5:565)
+
+
+
+
+
+selection <- select(all_combined, activity, subject,
+                    grep("mean()", names(all_combined)), 
+                    grep("std()", names(all_combined)))
+
+activity_data <- selection %>% group_by(activity, subject) %>% summarise_each(funs(mean))
+
+activity_gathered <- gather(activity_data, v1, v2, -activity, -subject)
+
+activity_separate <- separate(activity_gathered, v1, c("sensor", "statistic", "axis")) %>%
+  separate(sensor, c("domain", "sensor"), sep = 1) %>%
+  separate(sensor, c("acceleration signal", "sensor"), sep=4)
+
+activity_separate$domain <- gsub("t", "time", activity_separate$domain)
+activity_separate$domain <- gsub("f", "frequency", activity_separate$domain)
+
+activity_separate$`acceleration signal` <- gsub("Grav", "Gravity", activity_separate$`acceleration signal`)
+activity_separate$`acceleration signal` <- tolower(activity_separate$`acceleration signal`)
+
+activity_separate$sensor <- gsub("ity", "", activity_separate$sensor)
+activity_separate$sensor <- gsub("Body", "", activity_separate$sensor)
+
+activity_mutate <- mutate(activity_separate, sensor2 = sensor, sensor3 = sensor) %>%
+  select(1:5, 9, 10, 6:8)
+
+activity_mutate$sensor <- gsub("Jerk", "", activity_mutate$sensor)
+activity_mutate$sensor <- gsub("Mag", "", activity_mutate$sensor)
+activity_mutate$sensor2 <- gsub("Acc", "", activity_mutate$sensor2)
+activity_mutate$sensor2 <- gsub("Gyro", "", activity_mutate$sensor2)
+activity_mutate$sensor2 <- gsub("Mag", "", activity_mutate$sensor2)
+activity_mutate$sensor3 <- gsub("Acc", "", activity_mutate$sensor3)
+activity_mutate$sensor3 <- gsub("Jerk", "", activity_mutate$sensor3)
+activity_mutate$sensor3 <- gsub("Gyro", "", activity_mutate$sensor3)
+activity_mutate$sensor <- tolower(activity_mutate$sensor)
+activity_mutate$sensor2 <- tolower(activity_mutate$sensor2)
+activity_mutate$sensor3 <- tolower(activity_mutate$sensor3)
+activity_mutate$activity <- tolower(activity_mutate$activity)
+
+activity_mutate <- activity_mutate %>% rename("jerk signal" = sensor2) %>% rename(magnitude = sensor3)
+
+activity_filtered <- filter(activity_mutate, statistic != "meanFreq")
+
+tidy_data <- spread(activity_filtered, statistic, v2)
+
+tidy_data$activity <- as.factor(tidy_data$activity)
+tidy_data$domain <- as.factor(tidy_data$domain)
+tidy_data$`acceleration signal` <- as.factor(tidy_data$`acceleration signal`)
+tidy_data$sensor <- as.factor(tidy_data$sensor)
+tidy_data$`jerk signal` <- as.factor(tidy_data$`jerk signal`)
+tidy_data$magnitude <- as.factor(tidy_data$magnitude)
+tidy_data$axis <- as.factor(tidy_data$axis)
+
+levels(tidy_data$`jerk signal`) <- c(NA, "jerk")
+levels(tidy_data$magnitude) <- c(NA, "magnitude")
+levels(tidy_data$axis) <- c(NA, "X", "Y", "Z")
+
+write.table()
